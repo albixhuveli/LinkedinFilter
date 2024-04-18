b5350a60c3996f9df1395a9609266ddbd2220648
albixhuveli <albi.xhuveli.601@gmail.com>
2024-04-17 20:55:40 -0400

    added layout properly for tampermonkey extension

---
+++ Filter.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LinkedIn Jobs Blocklist
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  LinkedIn Jobs Filter
+// @author       Albi Xhuveli
+// @match        https://www.linkedin.com/jobs/*
+// @grant        none
+// @require      
+// @require      
+// ==/UserScript==
