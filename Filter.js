cca4aac43252aeee3e1c8cde1037bec5645779b5
albixhuveli <albi.xhuveli.601@gmail.com>
2024-04-18 23:34:24 -0400

    created script need to figure out tampermonkey ext next

--- Filter.js
+++ Filter.js
@@ -9,3 +9,32 @@
 // @require      
 // @require      
 // ==/UserScript==
+(function () {
+    'use strict';
+
+    var blocklist = [
+        "Company#1",
+        "Company#2",
+        "Company#3",
+    ];
+
+    blocklist = blocklist.map(function (item) {
+        return item.toLowerCase();
+    });
+
+    function filter(jNode) {
+        let company = jNode.find('.job-card-container__company-name').text().trim().toLowerCase();
+        if (blocklist.includes(company)) {
+            console.log('Hide item from company: ' + company);
+            jNode.closest(".artdeco-list__item").hide();
+        }
+
+        // to debug section
+        // console.log('Total items: ' + $('.job-card-container__company-name').length);
+        // console.log('Remaining items: ' + $('.job-card-container__company-name:visible').length);
+        // console.log('Hidden items: ' + $('.job-card-container__company-name:hidden').length);
+    }
+
+    waitForKeyElements(".job-card-container--clickable", filter);
+
+})();
